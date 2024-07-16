0c74e3e69d9b98b3ede6ba8b8c85e52525f18c8d
Xavier Arce <xavierarce54321@gmail.com>
2024-07-16 21:32:20 +0200

    ✨ Added fetching conditions

--- php/admin/update_user.php
+++ php/admin/update_user.php
@@ -1,15 +1,22 @@
 <?php
-include 'config.php';
+include '../config.php';
+header('Content-Type: application/json');
 session_start();
 
+if ($_SESSION['role'] !== 3) {
+  echo json_encode(['success' => false, 'error' => 'You do not have permission to perform this action']);
+  http_response_code(403);
+  exit();
+}
+
 $data = json_decode(file_get_contents('php://input'), true);
-$userId = $data['id'];
+$userId = $data['user_id'];
 $role = $data['role'];
 $banned = $data['banned'];
 $deleted = $data['deleted'];
 
 try {
-  $sql = "UPDATE users SET role = :role, banned = :banned, deleted = :deleted WHERE id = :userId";
+  $sql = "UPDATE users SET role = :role, banned = :banned, deleted = :deleted WHERE user_id = :userId";
   $stmt = $pdo->prepare($sql);
   $stmt->bindParam(':role', $role, PDO::PARAM_STR);
   $stmt->bindParam(':banned', $banned, PDO::PARAM_BOOL);
