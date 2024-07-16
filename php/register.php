775648f9eba83159d447d79551ee5ce0e8d14400
Xavier Arce <xavierarce54321@gmail.com>
2024-07-16 21:31:48 +0200

    ✨ Added bcrypt

--- php/register.php
+++ php/register.php
@@ -1,11 +1,10 @@
 <?php
+include 'config.php';
+include 'sessionUtils.php';
+
 session_start();
 header('Content-Type: application/json');
 
-include 'config.php'; // Ensure this file contains PDO initialization
-include 'sessionUtils.php'; // Ensure this file contains session utility functions like destroySessionWithMessage
-
-// Function to check if nickname or login already exists
 function checkIfExists($pdo, $field, $value)
 {
   $checkSql = "SELECT COUNT(*) AS count FROM users WHERE $field = :value";
@@ -15,23 +14,14 @@ function checkIfExists($pdo, $field, $value)
   $result = $checkStmt->fetch(PDO::FETCH_ASSOC);
   return $result['count'] > 0;
 }
-
-// Fetching user data from JSON input
 $data = json_decode(file_get_contents('php://input'), true);
-
-if (!isset($data['nickname'])) {
-  destroySessionWithMessage('Nickname not provided');
-  exit;
+$requiredFields = ['nickname', 'login', 'password'];
+foreach ($requiredFields as $field) {
+  if (!isset($data[$field])) {
+    destroySessionWithMessage(ucfirst($field) . ' not provided');
+    exit;
+  }
 }
-if (!isset($data['login'])) {
-  destroySessionWithMessage('Login not provided');
-  exit;
-}
-if (!isset($data['password'])) {
-  destroySessionWithMessage('Password not provided');
-  exit;
-}
-
 $nickname = trim($data['nickname']);
 $password = trim($data['password']);
 $login = trim($data['login']);
@@ -45,23 +35,19 @@ if (strlen($nickname) < 3 || strlen($login) < 3 || strlen($password) < 6) {
 }
 
 try {
-  // Check if the nickname already exists
+  function handleDuplicateError($field)
+  {
+    destroySession();
+    http_response_code(400);
+    echo json_encode(['success' => false, 'message' => "$field already exists"]);
+    exit();
+  }
   if (checkIfExists($pdo, 'nickname', $nickname)) {
-    destroySession();
-    http_response_code(400);
-    echo json_encode(['success' => false, 'message' => 'Nickname already exists']);
-    exit();
+    handleDuplicateError('Nickname');
+  } elseif (checkIfExists($pdo, 'login', $login)) {
+    handleDuplicateError('Login');
   }
 
-  // Check if the login already exists
-  if (checkIfExists($pdo, 'login', $login)) {
-    destroySession();
-    http_response_code(400);
-    echo json_encode(['success' => false, 'message' => 'Login already exists']);
-    exit();
-  }
-
-  // Inserting user data into the database
   $sql = "INSERT INTO users (nickname, login, password) VALUES (:nickname, :login, :password)";
   $stmt = $pdo->prepare($sql);
   $stmt->bindParam(':nickname', $nickname);
@@ -77,13 +63,8 @@ try {
     $response = [
       'success' => true,
       'message' => 'Registration successful!',
-      'userPSW' => $password,
-      'passwrd' => $hashed_password,
-      'compared' => password_verify($password, $hashed_password),
-      'compared1' => password_verify($password, $hashed_password),
-      'compared2' => password_verify($password, $hashed_password),
     ];
-    http_response_code(200); // 200 OK
+    http_response_code(200);
   } else {
     destroySession();
 
@@ -91,7 +72,7 @@ try {
       'success' => false,
       'message' => 'Registration failed. Please try again.'
     ];
-    http_response_code(500); // 500 Internal Server Error
+    http_response_code(500);
   }
 
   echo json_encode($response);
