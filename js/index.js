d18657cc0cec066c3eade566304d25c25c243e89
Xavier Arce <xavierarce54321@gmail.com>
2024-07-16 21:30:40 +0200

    ✨ ADDED FETCHING CONDITIONS

--- js/index.js
+++ js/index.js
@@ -6,49 +6,39 @@ import { fetchArticles } from "./components/articles/articles.js";
 import { onSetSession } from "./components/navBars/sideBar.js";
 import { fetchUsers } from "./admin.js";
 
-// Initialize modals
 addModals();
 
-//sidebar buttons
 const sidebarButtons = document.querySelectorAll(".sidebar-button");
 sidebarButtons.forEach((button) => {
   onSetSession(button);
 });
 
-// Get modal elements
 var loginModal = document.getElementById("loginModal");
 var registerModal = document.getElementById("registerModal");
 
-// Get buttons to open modals
 var loginBtn = document.getElementById("loginBtn");
 var registerBtn = document.getElementById("registerBtn");
-var createArticleBtn = document.getElementById("createArticleBtn"); // Assuming you have a button for creating articles
+var createArticleBtn = document.getElementById("createArticleBtn");
 
-// Get <span> elements that close the modals
 var closeLoginModal = document.getElementById("closeLoginModal");
 var closeRegisterModal = document.getElementById("closeRegisterModal");
 
-// When the user clicks the button, open the login modal
 loginBtn.onclick = function () {
   loginModal.style.display = "block";
 };
 
-// When the user clicks the button, open the register modal
 registerBtn.onclick = function () {
   registerModal.style.display = "block";
 };
 
-// When the user clicks on <span> (x), close the login modal
 closeLoginModal.onclick = function () {
   loginModal.style.display = "none";
 };
 
-// When the user clicks on <span> (x), close the register modal
 closeRegisterModal.onclick = function () {
   registerModal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == loginModal) {
     loginModal.style.display = "none";
@@ -58,16 +48,13 @@ window.onclick = function (event) {
   }
 };
 
-// Event listener for create article button click
 if (createArticleBtn) {
   createArticleBtn.addEventListener("click", openCreateArticleModal);
 }
 
-// Function to handle logout
 async function handleLogout() {
   const loggedOut = await logout();
   if (loggedOut) {
-    // Update UI after logout (if needed)
     document.getElementById("nickname").textContent = "";
     document.getElementById("nickname").style.display = "none";
     document.getElementById("logoutBtn").style.display = "none";
@@ -76,14 +63,14 @@ async function handleLogout() {
   }
 }
 
-// Event listener for logout button click
 document.getElementById("logoutBtn").addEventListener("click", handleLogout);
 
-// Check session when the page loads
 document.addEventListener("DOMContentLoaded", async () => {
   let response = await checkSession();
-  await fetchArticles();
   const currentUrl = window.location.href;
+  if (!currentUrl.includes("/admin")) {
+    await fetchArticles();
+  }
   if (currentUrl.includes("/admin")) {
     if (response?.role !== 3) {
       return (window.location.href = "/blog");
@@ -92,5 +79,4 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
-// Setup form handlers
 setupFormHandlers();
