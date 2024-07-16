9de2e0e5f819c94255bd21566ad585cef13e8d4c
Xavier Arce <xavierarce54321@gmail.com>
2024-07-16 21:31:45 +0200

    ✨ Added bcrypt

--- php/login.php
+++ php/login.php
@@ -1,71 +1,62 @@
 <?php
+include 'config.php';
+include 'sessionUtils.php';
+
 session_start();
 header('Content-Type: application/json');
 
-include 'config.php'; // Ensure this file contains PDO initialization
-include 'sessionUtils.php'; // Ensure this file contains session utility functions like destroySessionWithMessage
-
-// Fetching user data from JSON input
 $data = json_decode(file_get_contents('php://input'), true);
-
-if (!isset($data['login'])) {
-  destroySessionWithMessage('Login not provided');
-  exit;
+$requiredFields = ['login', 'password'];
+foreach ($requiredFields as $field) {
+  if (!isset($data[$field])) {
+    destroySessionWithMessage(ucfirst($field) . ' not provided');
+    exit;
+  }
 }
-if (!isset($data['password'])) {
-  destroySessionWithMessage('Password not provided');
-  exit;
-}
-
 $login = trim($data['login']);
 $password = trim($data['password']);
 
 try {
-  // Checking user in database
-  $sql = "SELECT nickname, user_id, role, password FROM users WHERE login = :login";
+  $sql = "SELECT * FROM users WHERE login = :login";
   $stmt = $pdo->prepare($sql);
   $stmt->bindParam(':login', $login);
   $stmt->execute();
 
-  if ($stmt->rowCount() > 0) {
-    $user = $stmt->fetch(PDO::FETCH_ASSOC); // Fetch the user data
-
-    if (password_verify($password, $user['password'])) {
-      $_SESSION['nickname'] = $user['nickname'];
-      $_SESSION['user_id'] = $user['user_id'];
-      $_SESSION['role'] = $user['role'];
-      $_SESSION['section'] = 'public';
-      $response = [
-        'success' => true,
-        'message' => 'Login successful!',
-        'session_user_id' => $_SESSION['user_id'],
-        'user' => $user,
-      ];
-      http_response_code(200); // 200 OK
-    } else {
-      destroySession();
-
-      $response = [
-        'success' => false,
-        'message' => 'Invalid password',
-        'userPSW' => $user['password'],
-        'passwrd' => $password,
-        'compared' => password_verify($password, $user['password']),
-
-      ];
-      http_response_code(401); // 401 Unauthorized
-    }
-  } else {
-    destroySession();
-
-    $response = [
-      'success' => false,
-      'message' => 'Invalid nickname',
-    ];
-    http_response_code(401); // 401 Unauthorized
+  if ($stmt->rowCount() === 0) {
+    handleInvalidLogin();
   }
 
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+  if (!password_verify($password, $user['password'])) {
+    handleInvalidLogin();
+  }
+
+  $_SESSION['nickname'] = $user['nickname'];
+  $_SESSION['user_id'] = $user['user_id'];
+  $_SESSION['role'] = $user['role'];
+  $_SESSION['section'] = 'public';
+
+  $response = [
+    'success' => true,
+    'message' => 'Login successful!',
+    'deleted' => $user['deleted'],
+    'banned' => $user['banned'],
+  ];
+  http_response_code(200);
+
   echo json_encode($response);
+
+  function handleInvalidLogin()
+  {
+    destroySession();
+    $response = [
+      'success' => false,
+      'message' => 'Invalid credentials',
+    ];
+    http_response_code(401);
+    echo json_encode($response);
+    exit;
+  }
 } catch (PDOException $e) {
   destroySession();
   http_response_code(500);
